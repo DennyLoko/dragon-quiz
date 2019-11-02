69483dc70bd1f55cbefc3e59c82fa081f392e432
Danniel Magno <dennyloko@gmail.com>
2019-11-01 22:10:32 -0300

    Adiciona DI

--- public/index.php
+++ public/index.php
@@ -3,5 +3,19 @@ declare(strict_types=1);
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-$helloWorld = new \DragonQuiz\Controller\HelloWorld();
+use \DI\ContainerBuilder;
+use function DI\create;
+use \DragonQuiz\Controller\HelloWorld;
+
+$containerBuilder = new ContainerBuilder();
+$containerBuilder->useAutowiring(false);
+$containerBuilder->useAnnotations(false);
+
+$containerBuilder->addDefinitions([
+    HelloWorld::class => create(HelloWorld::class)
+]);
+
+$container = $containerBuilder->build();
+
+$helloWorld = $container->get(HelloWorld::class);
 $helloWorld->run();
