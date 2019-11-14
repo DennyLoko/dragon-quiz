bf8b914574161ec6f5c40caddd36f767942734e4
Danniel Magno <dennyloko@gmail.com>
2019-11-13 21:42:03 -0300

    Adiciona biblioteca de conexão ao banco de dados

--- src/controller/HelloWorld.php
+++ src/controller/HelloWorld.php
@@ -2,6 +2,7 @@
 
 namespace DragonQuiz\Controller;
 
+use Doctrine\ORM\EntityManager;
 use Psr\Http\Message\ResponseInterface;
 use Twig\Environment;
 
@@ -9,10 +10,12 @@ class HelloWorld extends Controller
 {
     private $response;
     private $twig;
+    private $em;
 
-    public function __construct(ResponseInterface $response, Environment $twig) {
+    public function __construct(ResponseInterface $response, Environment $twig, EntityManager $em) {
         $this->response = $response;
         $this->twig = $twig;
+        $this->em = $em;
     }
 
     public function __invoke(): ResponseInterface
