0f6d8e878fdbd845341b1465b358c2a2ca8507a2
GrabK5 <gamervicio66@gmail.com>
2019-11-18 19:10:36 -0200

    renomeei .env

--- public/index.php
+++ public/index.php
@@ -47,7 +47,7 @@ $container = $containerBuilder->build();
 
 $routes = simpleDispatcher(function (RouteCollector $r) {
     $r->get('/', HelloWorld::class);
-    $r->get('')
+    $r->get('');
 });
 
 $middlewareQueue[] = new FastRoute($routes);
