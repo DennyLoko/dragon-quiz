de8108bdef6ad2210fd2d21d641fde1f8c587fe5
Danniel Magno <dennyloko@gmail.com>
2019-11-25 01:56:46 -0300

    Corrige codestyle e mapeamento das entidades

--- src/entity/Score.php
+++ src/entity/Score.php
@@ -13,13 +13,13 @@ class Score
 {
     /**
      * @ORM\Id
+     * @ORM\Column(type="integer")
      * @ORM\GeneratedValue
-     * @ORM\Column(name="id")
      */
     protected $id;
 
     /**
-     * @ORM\Column(name="points")
+     * @ORM\Column(name="integer")
      */
     protected $points;
 
@@ -32,53 +32,46 @@ class Score
     /**
      * @return mixed
      */
-    public function getId()
-    {
+    public function getId() {
         return $this->id;
     }
 
     /**
      * @param mixed $id
      */
-    public function setId($id)
-    {
+    public function setId($id) {
         $this->id = $id;
     }
 
     /**
      * @return mixed
      */
-    public function getPoints()
-    {
+    public function getPoints() {
         return $this->points;
     }
 
     /**
      * @param mixed $points
      */
-    public function setPoints($points)
-    {
+    public function setPoints($points) {
         $this->points = $points;
     }
 
     /**
      * @return mixed
      */
-    public function getUser()
-    {
+    public function getUser() {
         return $this->user;
     }
 
     /**
      * @param mixed $user
      */
-    public function setUser($user)
-    {
+    public function setUser($user) {
         $this->user = $user;
     }
 
-    public function incrementScore(int $points)
-    {
+    public function incrementScore(int $points) {
         $this->points += $points;
     }
 }
